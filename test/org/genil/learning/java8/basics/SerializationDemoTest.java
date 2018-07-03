5fb4af01328f51bf024779c46243d48c341a37eb
TonyGregg <antonygenil@gmail.com>
2018-07-03 06:51:05 -0400

    Serialization  - custom serializtion added

---
+++ test/org/genil/learning/java8/basics/SerializationDemoTest.java
@@ -0,0 +1,17 @@
+package org.genil.learning.java8.basics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by anton on 7/3/2018 6:39 AM
+ **/
+class SerializationDemoTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
